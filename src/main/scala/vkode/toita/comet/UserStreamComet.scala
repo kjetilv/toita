c0480c4afe2b41c57e533f3037f0a3202eaebd42
Kjetil Valstadsve <valstadsve@gmail.com>
2011-01-11 22:08:20 +0100

    Ajax in the comet.

--- src/main/scala/vkode/toita/comet/UserStreamComet.scala
+++ src/main/scala/vkode/toita/comet/UserStreamComet.scala
@@ -1,15 +1,19 @@
 package vkode.toita.comet
 
 import scala.xml._
+import scala.math._
 
 import net.liftweb.common._
-import net.liftweb.http.CometActor
-
 import vkode.toita.backend._
 import net.liftweb.http.js.JsCmds._
 import java.util.Date
 import scalaz.Options
 import org.joda.time.DateTime
+import net.liftweb.http.{DispatchSnippet, RequestVar, S, SHtml, SessionVar, CometActor}
+import S._
+import SHtml._
+
+object Index extends SessionVar[Int](0)
 
 class UserStreamComet
     extends CometActor with Options with ToitaRegister with ToitaSessionUser with Loggable {
@@ -20,16 +24,22 @@ class UserStreamComet
 
   private var conversation: Option[Conversation[TwitterStatusUpdate]] = None
 
-  override def render = bind("us",
-                             "tweets" -> renderTable,
-                             "friends" -> renderFriends)
-
-  def renderFriends: NodeSeq =
-    Rendrer render (friends map (_ friends) map (_.friends))
+  override def render =
+    bind("us",
+         "next" -> ajaxButton("Next", () => {
+           Index(Index.get + 1)
+           SetHtml ("tweets", renderTable)
+         }),
+         "previous" -> ajaxButton("Previous", () => {
+           Index(math.max(Index.get - 1, 0))
+           SetHtml ("tweets", renderTable)
+         }),
+         "index" -> Text(Index.get + ""),
+         "tweets" -> renderTable)
 
   def renderTable: NodeSeq =
     try {
-      conversation map (conv => Rendrer render (conv.items take 250)) getOrElse <span>Waiting for conversation...</span>
+      conversation map (conv => Rendrer render (conv.items drop (Index.get * 10) take 100)) getOrElse <span>Waiting for conversation...</span>
     } catch {
       case e =>
         logger.warn("Failed to render table!", e)
@@ -49,10 +59,6 @@ class UserStreamComet
     case update: Conversation[TwitterStatusUpdate] =>
       conversation = Option(update)
       rerenderTable
-    case friends: TwitterFriends =>
-      this.friends = Some(friends)
-      partialUpdate(SetHtml("friends", renderFriends))
-      reRender
   }
 
   override def toString = getClass.getSimpleName + "[name:" + name + " " + System.identityHashCode(this) + "]"
--- src/main/scala/vkode/toita/snippet/User.scala
+++ src/main/scala/vkode/toita/snippet/User.scala
@@ -8,7 +8,8 @@ object User extends ToitaSessionUser {
 
   lazy val user = theTwitter.user
 
-  def render = ("#screenName" #> (user map (_.screen_name) getOrElse "[unknown]") &
+  def render = ("#url" #> (user map (_.profile_image_url) getOrElse ("./images/ajax-loader.gif")) &
+                "#screenName" #> (user map (_.screen_name) getOrElse "[unknown]") &
                 "#name" #> (user map (_.name) getOrElse "[unknown]") &
                 "#description" #> (user map (_.description) getOrElse None getOrElse "[unknown]"))
 }
