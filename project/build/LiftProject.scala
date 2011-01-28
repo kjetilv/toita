ebfbacce06fa4b6da3ec71fae94d0311a343bd3d
Kjetil Valstadsve <valstadsve@gmail.com>
2011-01-28 23:33:25 +0100

    Getting it back to working order. Damn dependencies.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -5,7 +5,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   override def repositories = super.repositories +
                               ("Scala-Tools Dependencies Repository for Releases" at "http://scala-tools.org/repo-releases") +
                               ("Scala-Tools Dependencies Repository for Snapshots" at "http://scala-tools.org/repo-snapshots") +
-                              ("Akka Maven2 Repository" at "http://www.scalablesolutions.se/akka/repository") +
+                              ("Akka Maven2 Repository" at "http://akka.io/repository/") +
                               ("Multiverse Maven2 Repository" at "http://multiverse.googlecode.com/svn/maven-repository/releases") +
                               ("GuiceyFruit Maven2 Repository" at "http://guiceyfruit.googlecode.com/svn/repo/releases/") +
                               ("JBoss Maven2 Repository" at "https://repository.jboss.org/nexus/content/groups/public/")
@@ -34,8 +34,8 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   sbtDepend("net.liftweb", "2.2", "lift-" + _, "mapper", "actor", "json", "webkit")
   sbtDepend("com.googlecode.scalaz", "5.0", "scalaz-" + _, "core", "http")
 
-  depend("se.scalablesolutions.akka", "1.0-RC5", "akka-" + _, "actor", "core")
-  depend("com.eaio", "3.2", "uuid")
+  depend("se.scalablesolutions.akka", "1.0-RC3", "akka-" + _, "actor", "core")
+//  depend("com.eaio", "3.2", "uuid")
   depend("com.h2database", "1.2.138", "h2")
   depend("junit", "4.8.2", "junit")
   depend("org.scribe", "1.0.9", "scribe")
