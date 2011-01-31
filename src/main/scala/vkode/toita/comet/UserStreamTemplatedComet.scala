84157eb84dcf35ecb5e84c8412a7b8f463a3dfde
Kjetil Valstadsve <valstadsve@gmail.com>
2011-01-31 21:10:09 +0100

    Another way to render - using templates.

--- src/main/scala/vkode/toita/comet/UserStreamTemplatedComet.scala
+++ src/main/scala/vkode/toita/comet/UserStreamTemplatedComet.scala
@@ -17,19 +17,31 @@ class UserStreamTemplatedComet extends UserStream {
 
   private var friends: Option[TwitterFriends] = None
 
-  private def tweetarea = findElems (defaultXml) (_.attribute("id").map(_ == "tweetarea") getOrElse false)
-
-  override def render = {
-    logger.info(this + " found tweetarea: " + tweetarea)
-    bind("us", tweetarea,
-         "img" -> Text("img"),
-         "name" -> Text("name"),
-         "text" -> Text("text"))
+  private def bindItem(item: StreamItem[TwitterStatusUpdate]): NodeSeq = item match {
+    case StreamItem(TwitterStatusUpdate(TOStatus(sid, text),
+                                        meta,
+                                        Some(TOUser(uid, screen_name, name, _, _, _, imageUrl)),
+                                        retweet,
+                                        _,
+                                        _,
+                                        deleted,
+                                        _),
+                    _, _, _, _, _, _) =>
+      bind("us", defaultXml,
+           "img" -> <img src={ imageUrl } alt={ name } width="48" height="48"/>,
+           "name" -> Text(name),
+           "text" -> Text(text))
+    case x => <span>Ooops: {x.toString}</span>
   }
 
+  private def renderStream(items: List[StreamItem[TwitterStatusUpdate]]): NodeSeq = items flatMap (bindItem _)
+
+  private def renderOption: NodeSeq = stream map (_ items) map (_ take 5) map (renderStream _) getOrElse Text("Loading ...")
+
+  override def render = renderOption
+
   protected def updated {
-    stream map(_.items) map (_ flatMap (item => {
-      Text(item.toString)
-    }))
+    partialUpdate(SetHtml("tweetarea", renderOption))
+    reRender(false)
   }
 }
